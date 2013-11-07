301213c820802bc8aa85f8df17322309d69ace3c
Punit Soni <punitxsmart@gmail.com>
2013-11-07 00:05:40 -0800

    add rasp-pi interfacing test code

--- rv_arduino.ino
+++ rv_arduino.ino
@@ -1,19 +1,33 @@
 #include <Wire.h>
 
-#define SLAVE_ADDRESS 0x04
+#define SLAVE_ADDRESS 0x11
 
 int number = 0;
 int state = 0;
 
+/* state variables */
+int lspeed = 0;
+int rspeed = 0;
+
+/* commands and parameters */
+int CMD_SET = 0;
+int CMD_GET = 0;
+
+int P_RV_SPEED = 0;
+int P_SERVO_POS = 1;
+
+unsigned long reply = 0;
+int reply_idx = 0;
+
 void setup() {
     pinMode(13, OUTPUT);
-    Serial.begin(9600);         // start serial for output
+    Serial.begin(9600);
     // initialize i2c as slave
     Wire.begin(SLAVE_ADDRESS);
 
     // define callbacks for i2c communication
-    Wire.onReceive(receiveData);
-    Wire.onRequest(sendData);
+    Wire.onReceive(receiveDataOnI2C);
+    Wire.onRequest(sendDataToI2C);
 
     Serial.println("Ready!");
 }
@@ -23,29 +37,32 @@ void loop() {
 }
 
 // callback for received data
-void receiveData(int byteCount){
-
+void receiveDataOnI2C(int byteCount) {
+  int data = 0;
+  int i = 0;
+  int b;
+  digitalWrite(13, HIGH);
     while(Wire.available()) {
-        number = Wire.read();
+        b = Wire.read();
         Serial.print("data received: ");
-        Serial.println(number);
-
-        if (number == 1){
-
-            if (state == 0){
-                digitalWrite(13, HIGH); // set the LED on
-                state = 1;
-            }
-            else{
-                digitalWrite(13, LOW); // set the LED off
-                state = 0;
-            }
-         }
+        Serial.println(b);
+        //if(i < 4)
+        //  data += b << 8*i;
+        data = b;
+        i++;
      }
+     reply = data;
+     digitalWrite(13, LOW);
 }
 
 // callback for sending data
-void sendData(){
-    Wire.write(number);
+void sendDataToI2C() {
+  
+    //int b = (reply >> (reply_idx*8)) & 0xff;
+    int b = reply;
+    Wire.write(b);
+    reply_idx ++;
+    if(reply_idx >= 4)
+      reply_idx = 0;
 }
 
