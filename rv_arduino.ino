e69455b6d10e86939b4bd6de8f7a55decac15226
Punit Soni <punitxsmart@gmail.com>
2013-11-10 17:47:43 -0800

    enable i2c, pwm and encoders

--- rv_arduino.ino
+++ rv_arduino.ino
@@ -1,10 +1,53 @@
+#include <Timer.h>
+#include <PinChangeInt.h>
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x11
 
-/* state variables */
-int lspeed = 0;
-int rspeed = 0;
+#define sign(x) ((x > 0) ? 1 : -1)
+
+#define DIR_FWD   1
+#define DIR_BACK  0
+
+#define LEFT  0
+#define RIGHT 1
+
+#define PIN_ENC0_INT A0
+#define PIN_ENC1_INT A1
+#define PIN_ENC2_INT A2
+#define PIN_ENC3_INT A3
+
+#define SPEED_UPDATE_INTERVAL 40
+#define SPEED_MAX_PPS         300
+#define SPEED_TOLERANCE       5
+
+#define CH_LEFT0   1
+#define CH_LEFT1   3
+#define CH_RIGHT0  0
+#define CH_RIGHT1  2
+
+
+Timer t;
+int lspeed = 0, rspeed = 0;
+int prev_lspeed = 0, prev_rspeed = 0;
+
+volatile unsigned int pulse_count[4] = {0, 0, 0, 0};
+volatile int cur_speed[4];
+volatile int req_speed[4];
+volatile unsigned char cur_pwm[4];
+volatile int cur_err[4];
+
+
+/* motor channels 1, 2, 3, 4
+ * left channels 2, 4
+ * right channels 1, 3
+ */
+int pins_pwm[] = {9, 6, 5, 3};
+int pins_left_pwm[] = {6, 3};
+int pins_right_pwm[] = {9, 5};
+int pin_left_dir = 4;
+int pin_right_dir = 7;
+
 
 /* commands and parameters */
 enum {
@@ -22,23 +65,133 @@ void setup() {
     Wire.begin(SLAVE_ADDRESS);
     Wire.onReceive(receiveDataOnI2C);
     Wire.onRequest(sendDataToI2C);
+    init_motors();
+    init_encoders();
+    t.every(SPEED_UPDATE_INTERVAL, measure_speed, (void*) 0);
+    t.every(500, display_serial, (void*) 0);
+
     Serial.println("RV Controller Interface: Ready");
 }
 
 void loop() {
-    delay(100);
+    t.update();
+    correct_speed();
 }
 
-int processCommand(int cmd, int data) {
+void display_serial(void *arg) {
+  Serial.print("cur_speed, cur_pwm, cur_err: ");
+  Serial.print(cur_speed[0]);
+  Serial.print(" ");
+  Serial.print(cur_pwm[0]);
+  Serial.print(" ");
+  Serial.print(cur_err[0]);
+  Serial.println(" ");
+}
+void measure_speed(void *arg) {
+  int i, pps;
+  for(i=0; i< 1; i++) {
+    pps = pulse_count[i] * (1000 / SPEED_UPDATE_INTERVAL);
+    cur_speed[i] = (pps * 100) / SPEED_MAX_PPS;
+    pulse_count[i] = 0;
+  }
+}
+
+void correct_speed() {
+  int i, err;
+  for(i=0; i<1; i++) {
+    cur_err[i] = abs(cur_speed[i]) - abs(req_speed[i]);
+    if(abs(cur_err[i]) > SPEED_TOLERANCE) {
+      cur_pwm[i] = cur_pwm[i] + sign(err)*1;
+      analogWrite(pins_pwm[0], cur_pwm[i]);
+    }
+  }
+}
+
+
+void init_encoders()
+{
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  PCintPort::attachInterrupt(PIN_ENC0_INT, &enc0isr, RISING);
+  PCintPort::attachInterrupt(PIN_ENC1_INT, &enc1isr, RISING);
+  PCintPort::attachInterrupt(PIN_ENC2_INT, &enc2isr, RISING);
+  PCintPort::attachInterrupt(PIN_ENC3_INT, &enc3isr, RISING);
+}
+
+void enc0isr() {
+  pulse_count[0]++;
+}
+
+void enc1isr() {
+  pulse_count[1]++;
+}
+
+void enc2isr() {
+  pulse_count[2]++;
+}
+
+void enc3isr() {
+  pulse_count[3]++;
+}
+
+void init_motors()
+{
+  int i;
+  for(i=0; i<4; i++) {
+    pinMode(pins_pwm[i], OUTPUT); 
+  }
+  pinMode(pin_left_dir, OUTPUT);
+  pinMode(pin_right_dir, OUTPUT);
+}
+
+/* updates rover left and right speed */
+void updateMovement()
+{
+  int val, dir;
+  if(lspeed != prev_lspeed) {
+    val = (lspeed * 255)/100;
+    if(lspeed < 0) {
+      val = -val;
+      dir = DIR_BACK;
+    } else {
+      dir = DIR_FWD;
+    }
+    analogWrite(pins_left_pwm[0], val & 0xff);
+    analogWrite(pins_left_pwm[1], val & 0xff);
+    digitalWrite(pin_left_dir, dir);
+    prev_lspeed = lspeed;
+  }
+  if(rspeed != prev_rspeed) {
+    val = (rspeed * 255)/100;
+    if(rspeed < 0) {
+      val = -val;
+      dir = DIR_BACK;
+    } else {
+      dir = DIR_FWD;
+    }
+    analogWrite(pins_right_pwm[0], val & 0xff);
+    analogWrite(pins_right_pwm[1], val & 0xff);
+    digitalWrite(pin_right_dir, dir);
+    prev_rspeed = rspeed;
+  }
+}
+
+int processCommand(int cmd, unsigned int data) {
   int ret = 255;
   switch(cmd) {
     case CMD_SET_LSPEED:
-      lspeed = data;
+      lspeed = (signed char) data;
+      req_speed[CH_LEFT0] = lspeed;
+      req_speed[CH_LEFT1] = lspeed;
       Serial.print("lspeed = ");
       Serial.println(lspeed);
       break;
     case CMD_SET_RSPEED:
-      rspeed = data;
+      rspeed = (signed char) data;
+      req_speed[CH_RIGHT0] = rspeed;
+      req_speed[CH_RIGHT1] = rspeed;      
       Serial.print("rspeed = ");
       Serial.println(rspeed);       
       break;
